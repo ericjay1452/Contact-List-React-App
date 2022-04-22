893623b3819e80becc152e217ca4c966a717b3b1
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-04-22 22:35:57 +0100

    My first Commit on this App

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+      <h2 className='text-danger'>Waw</h2>
+  );
+}
+
+export default App;
